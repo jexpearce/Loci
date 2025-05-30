a294ab08502948bf60b8f856b7728bc91dcceb28
Jex Pearce <jex@jajajeev.com>
2025-05-30 15:58:56 +0100

    Initial Commit

---
+++ Loci/LociApp.swift
@@ -0,0 +1,17 @@
+//
+//  LociApp.swift
+//  Loci
+//
+//  Created by Jex Pearce on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct LociApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LociTests/LociTests.swift
@@ -0,0 +1,16 @@
+//
+//  LociTests.swift
+//  LociTests
+//
+//  Created by Jex Pearce on 30/05/2025.
+//
+
+import Testing
+
+struct LociTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
